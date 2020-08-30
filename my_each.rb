f4b3ca822f5b9e644467a927037053def6a1ef89
Haley Grassi <teacherhaleymarie@gmail.com>
2020-08-30 13:43:23 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,9 @@
 def my_each(arr)
-  # code here	  
-  i = 0
-  while i < arr.length
-    yield(arr[i])
-    i += 1
-  end
-  arr
+# code here	  
+i = 0
+while i < arr.length
+yield(arr[i])
+i += 1
+end
+arr
 end
